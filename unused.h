8b95d92048fde740c21c1e9ae516d6ace4c5bc73
Marcel Waldvogel <marcel.waldvogel@trifence.ch>
2020-06-29 19:58:08 +0200

    Explain name

--- unused.h
+++ unused.h
@@ -1,4 +1,9 @@
-// Inspired by https://jmmv.dev/2015/02/unused-parameters-in-c-and-c.html
-// and https://github.com/jmmv/kyua/blob/244eb9/utils/defs.hpp.in#L57
+/*
+ * This is *not* an unused header file; instead, it allows to declare
+ * parameters as being unused.
+ *
+ * Inspired by https://jmmv.dev/2015/02/unused-parameters-in-c-and-c.html
+ * and https://github.com/jmmv/kyua/blob/244eb9/utils/defs.hpp.in#L57
+ */
 #define UNUSED_ATTRIBUTE	__attribute__((unused))
 #define UNUSED_PARAM(name)	unused_ ## name UNUSED_ATTRIBUTE
